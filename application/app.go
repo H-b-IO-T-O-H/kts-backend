c3d53aed45dc38263b383a84f5806f9bfe856199
H-b-IO-T-O-H <importantv@yandex.ru>
2021-02-25 21:36:53 +0300

    update cookies

--- application/app.go
+++ application/app.go
@@ -100,8 +100,8 @@ func NewApp(config Config) *App {
 	}
 
 	store.Options(sessions.Options{
-		//Domain:   "localhost", // for postman
-		Domain: "135.181.207.76",
+		Domain:   "localhost", // for postman
+		//Domain: "135.181.207.76",
 		MaxAge:   int((3 * 12 * time.Hour).Seconds()),
 		Secure:   false,
 		HttpOnly: true,
